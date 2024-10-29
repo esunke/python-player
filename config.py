bc9876ca3b7d369c88400763791afad7e97a1c25
esunke <sunke@sucea.de>
2024-10-29 14:48:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 class Config:
     DEBUG = False
     TESTING = False
-    SECRET_KEY = 'your_secret_key'
     SERVER_NAME = 'localhost:5000'
     BLUETOOTH_SPEAKER_NAME = 'YourBluetoothSpeakerName'
-    MP3_FILE_PATH = 'path/to/your/sound.mp3'
+    MP3_FILE_PATH = 'scream.mp3'
